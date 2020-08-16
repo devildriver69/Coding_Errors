d4912ba48cbeaf56b7efe65c5a2a61a08df64248
devildriver69 <43845117+devildriver69@users.noreply.github.com>
2020-08-16 09:22:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include<bits/stdc++.h>
+using namespace std;
+bool f(int x,int y)
+{
+    return x>y;
+}
+int main()
+{
+    int n;cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int x;cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end(),f);
+    auto it=v.begin();
+    it++;
+    int max = *v.begin()*1;
+    for(int i=1;i<n;i++)
+    {
+        it++;
+        int cnt = it-v.begin();
+        if(v[i]*cnt>max)
+        {
+            max=v[i]*cnt;
+        }
+    }
+    cout<<max<<endl;
     return 0;
 }
